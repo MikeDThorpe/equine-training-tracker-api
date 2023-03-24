ccbf17a314b5f4eb91ab1b2718d0b00d3292a70b
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2023-03-24 14:46:03 +0000

    init project and structure

---
+++ src/main/java/uk/co/bransbyhorses/equinetrainingtrackerapi/EquineTrainingTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package uk.co.bransbyhorses.equinetrainingtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquineTrainingTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquineTrainingTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uk/co/bransbyhorses/equinetrainingtrackerapi/constants/ApiMappingConstants.java
@@ -0,0 +1,7 @@
+package uk.co.bransbyhorses.equinetrainingtrackerapi.constants;
+
+public class ApiMappingConstants {
+
+    /** privater constructor to prevent instance being created */
+    private ApiMappingConstants() {}
+}
