8ba9c746f97bc3b92f4147a5830e7fd6ab2c425f
Michael Thorpe <michaelthorpe747@hotmail.com>
2023-03-26 20:33:25 +0100

    4 - add support for Spring to mapper class

--- src/main/java/uk/co/bransbyhorses/equinetrainingtrackerapi/mappers/TrainingEnvironmentMapper.java
+++ src/main/java/uk/co/bransbyhorses/equinetrainingtrackerapi/mappers/TrainingEnvironmentMapper.java
@@ -1,10 +1,12 @@
 package uk.co.bransbyhorses.equinetrainingtrackerapi.mappers;
 
+import org.hibernate.annotations.Comment;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import uk.co.bransbyhorses.equinetrainingtrackerapi.api.models.TrainingEnvironment;
 import uk.co.bransbyhorses.equinetrainingtrackerapi.api.models.dto.TrainingEnvironmentDto;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface TrainingEnvironmentMapper {
 
     TrainingEnvironmentDto mapToDto(TrainingEnvironment trainingEnvironment);
